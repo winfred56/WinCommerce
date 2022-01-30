9350183de0d5ba90fc66d01da5bffb5b10a43300
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-01-30 08:42:52 +0000

    urls modified

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
+    path('', include('accounts.urls', namespace='accounts')),
 ]
 
 if settings.DEBUG:
