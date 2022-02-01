21e95d6457520572c387087dd06034b83b4856a4
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-01 07:14:46 +0000

    Update views.py (basket)

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from shop.models import Product
 from .models import Cart, CartItem
 
 
 def add_to_cart(request,slug):
-    
+    #Get the product to be added to the cart
+    product = get_object_or_404(Product, slug=slug)
+    #Get item if it's already in the cart_item or create a cart_item for the product
+    cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user, ordered=False)
+    #Filter according to the current logged in user and incomplete oreders
+    cart_ = Cart.objects.filter(user=request.user, ordered = False)
+    #If there are any incomplete orders for the current logged in user:
+    if cart_.exists():
+        cart = cart_[0]
+        if cart.products.filter(product__id=product.id).exists():
+            cart_item.quantity += 1
+            cart_item.save()
+        else:
+            cart.products.add(cart_item)
+    else:
+        #ordered_date = timezone.now()
+        cart = Cart.objects.create(user=request.user)
+        cart.products.add(cart_item)
+    return redirect("home")
