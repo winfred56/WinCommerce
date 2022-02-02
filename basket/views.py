d22cfd7e488fa0b5b3290924dfdc081d9a208cf0
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-02 11:45:30 +0000

    remove_from_cart

--- basket/views.py
+++ basket/views.py
@@ -25,3 +25,19 @@ def add_to_cart(request,slug):
         cart = Cart.objects.create(user=request.user, ordered_date=ordered_date)
         cart.products.add(cart_item)
     return redirect("/")
+
+def remove_from_cart(request, slug):
+    #Get the product to be added to the cart
+    product = get_object_or_404(Product, slug=slug)
+    cart_ = Cart.objects.filter(user=request.user, ordered = False)
+    if cart_.exists():
+        cart = cart_[0]
+        if cart.products.filter(product__id=product.id).exists():
+            cart_item = CartItem.objects.filter(product=product, user=request.user, ordered=False)[0]
+            cart_item.product.remove(cart_item)
+            return redirect('/')
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
+    return redirect("/")
