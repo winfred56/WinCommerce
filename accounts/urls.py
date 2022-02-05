5808b6b53cdc478ae5b05d1ec4e30e81aeab3048
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-05 12:46:11 +0000

    Create signup.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,8 @@ from django.contrib.auth import views as auth_views
 app_name = "accounts"
 
 urlpatterns = [
-    path('login/',auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name='login' ),
-    path('logout/',auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='logout' ),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('register/', views.Registration, name='register')
 ]
 
