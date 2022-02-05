91b206de7c670c490d8ad6220720212b4393868e
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-05 12:56:29 +0000

    account.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,3 @@
-from re import template
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -8,6 +7,5 @@ app_name = "accounts"
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-    path('register/', views.Registration, name='register')
+    path('register/', views.signup, name='register'),
 ]
-
