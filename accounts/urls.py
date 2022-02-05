c3c8c0b91379fa3306795148cb0f751dbca9f3dc
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-05 11:52:20 +0000

    correct loin and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name='login' ),
-    path('logout/',auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='login' ),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='logout' ),
 ]
 
