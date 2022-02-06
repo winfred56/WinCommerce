b70209b29570789471f964a0e67394cb7ba377f2
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-06 14:05:47 +0000

    accounts.models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned is_staff=True')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned is_superuser=True')
+        return self.create_superuser(email, user_name, password, **other_fields)
+
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email"), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    full_name = models.CharField(max_length=250)
+    about = models.TextField(_("about"), blank=True)
+
+    # Delivery Details
+    address_line1 = models.CharField(max_length=100, blank=True)
+    address_line2 = models.CharField(max_length=100, blank=True)
+    country = CountryField()
+    # User status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    object = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = 'Accounts'
+        verbose_name_plural = 'Accounts'
+
+    def __str__(self):
+        return self.user_name
