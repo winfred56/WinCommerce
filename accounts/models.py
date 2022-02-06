d009323caa31cc0d365aba836f44cca61e5ef13e
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-06 14:12:17 +0000

    create_user function(account.models)

--- accounts/models.py
+++ accounts/models.py
@@ -15,6 +15,15 @@ class CustomAccountManager(BaseUserManager):
             raise ValueError('Superuser must be assigned is_superuser=True')
         return self.create_superuser(email, user_name, password, **other_fields)
 
+    def create_user(self, email, user_name, password, **other_fields):
+        if not email:
+            raise ValueError('You must enter an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
 
 class UserBase(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email"), unique=True)
